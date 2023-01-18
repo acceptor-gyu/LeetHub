0f964f2a2ef7df289aa1a92b2241b1d5df30812a
kim seonggyu <71162390+acceptor-gyu@users.noreply.github.com>
2023-01-18 16:40:02 +0900

    Time: 11 ms (58.02%), Space: 41.5 MB (84.39%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,21 +1,8 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if (x < 0) {
-            return false;
-        }
+        String ori = String.valueOf(x);
+        String reversed = new StringBuilder(ori).reverse().toString();
 
-        int origin = x;
-        long beawareOfOverflow = 0;
-
-        while (x != 0) {
-            beawareOfOverflow = beawareOfOverflow * 10 + x % 10;
-            x = x / 10;
-        }
-
-        if ((int) beawareOfOverflow == origin) {
-            return true;
-        }
-
-        return false;
+        return ori.equals(reversed);
     }
 }
