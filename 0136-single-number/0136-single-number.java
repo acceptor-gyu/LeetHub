486c5c39f9032fa84239840c84f7b7e08c4eb542
kim seonggyu <71162390+acceptor-gyu@users.noreply.github.com>
2023-04-30 16:19:44 +0900

    Time: 12 ms (27.08%), Space: 42.9 MB (33.81%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        
+        Set<Integer> numSet = new HashSet<>();
+        
+        for (int num : nums) {
+            if (numSet.contains(num)) {
+                numSet.remove(num);
+            } else {
+                numSet.add(num);
+            }
+        }
+        
+        int answer = 0;
+        
+        for (int num : numSet) {
+            answer = num;
+        }
+        
+        return answer;
+    }
+}
