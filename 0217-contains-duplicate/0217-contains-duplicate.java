1dcad64775af0b40d2029ee77b13a41f4f78aca8
kim seonggyu <71162390+acceptor-gyu@users.noreply.github.com>
2023-05-03 10:39:41 +0900

    Time: 12 ms (78.01%), Space: 55.9 MB (73.89%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -4,10 +4,13 @@ class Solution {
         Set<Integer> set = new HashSet<>();
         
         for (int num : nums) {
+            if (set.contains(num)) {
+                return true;
+            }
             set.add(num);
         }
         
-        return set.size() != nums.length;
+        return false;
         
     }
 }
